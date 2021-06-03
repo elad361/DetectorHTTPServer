68a94ba24dd04089cbf64873714d307d028692af
elad361 <72965673+elad361@users.noreply.github.com>
2021-06-03 22:51:09 +0300

    Update Server.cpp

--- Server.cpp
+++ Server.cpp
@@ -1,39 +1,76 @@
 #include <string>
 #include <iostream>
 #include <thread>
+#include <sstream>
 #include "mongoose.h"
 #include "detectorFiles/SimpleAnomalyDetector.h"
 #include "detectorFiles/HybridAnomalyDetector.h"
 #include "detectorFiles/timeseries.h"
 
 static struct mg_serve_http_opts s_http_server_opts;
+using namespace std;
 
 struct Files {
 	std::string file1;
 	std::string file2;
 	std::string algo;
+	int size;
 };
 
+Files extractData(const char* buf) {
+	Files files;
+	std::stringstream s;
+	s << buf;
+	string line;
+	for (int i = 0; i < 9; i++) {
+		std::getline(s, line);
+		if (i == 4) {
+			files.size = stoi(line.substr(line.find(': '), line.length()));
+		}
+	}
+	std::stringstream newS(line.substr(0, files.size));
+	getline(newS, files.file1, ':');
+	getline(newS, files.file2, ':');
+	getline(newS, files.algo);
+	return files;
+}
+
+void convertVecrtorToJASON(std::vector<AnomalyReport>& reports, string& s) {
+	std::stringstream json;
+	json << "[" << endl;
+	for (AnomalyReport report : reports) {
+		json << " {" << std::endl;
+		json << "  \"timeStep\":" << report.timeStep << "," << endl;
+		json << "  \"columns\":" << report.description << endl;
+		json << " }," << endl;
+	}
+	json << "];";
+	s = json.str();
+}
+
 static void ev_handler(struct mg_connection* nc, int ev, void* p) {
-	Files* files = (Files *) &(nc->recv_mbuf);
-	TimeSeriesAnomalyDetector* detector;
-	if (files->algo.compare("hybrid")) {
-		detector = new HybridAnomalyDetector();
+	
+	if (ev == MG_EV_HTTP_REQUEST) {
+		Files files = extractData(nc->recv_mbuf.buf);
+		TimeSeriesAnomalyDetector* detector;
+		if (files.algo.compare("hybrid")) {
+			detector = new HybridAnomalyDetector();
+		}
+		else {
+			detector = new SimpleAnomalyDetector();
+		}
+		std::thread l([files, detector]() -> void {
+			TimeSeries t(files.file1.c_str());
+			detector->learnNormal(t); });
+		TimeSeries ts(files.file2.c_str());
+		l.join();
+		std::vector<AnomalyReport> v = detector->detect(ts);
+		string s;
+		convertVecrtorToJASON(v, s);
+		mg_send(nc, (void*)s.c_str(), sizeof(v));
+		delete detector;
+		//mg_serve_http(nc, (struct http_message*) p, s_http_server_opts);
 	}
-	else {
-		detector = new SimpleAnomalyDetector();
-	}
-	std::thread l([files, detector] () -> void {
-		TimeSeries t(files->file1.c_str());
-		detector->learnNormal(t); });
-	TimeSeries ts(files->file2.c_str());
-	l.join();
-	std::vector<AnomalyReport> v = detector->detect(ts);
-	mg_send(nc, v, sizeof(v));
-	delete detector;
-	/*if (ev == MG_EV_HTTP_REQUEST) {
-		mg_serve_http(nc, (struct http_message*) p, s_http_server_opts);
-	}*/
 }
 
 int initServer(int port) {
@@ -69,3 +106,5 @@ int main(void) {
 	initServer(port);
 	return 0;
 }
+
+
