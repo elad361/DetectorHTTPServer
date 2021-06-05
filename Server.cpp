3393a827f6c21460128303e60d3b4aec4e2cba59
elad361 <72965673+elad361@users.noreply.github.com>
2021-06-05 21:11:25 +0300

    Update Server.cpp

--- Server.cpp
+++ Server.cpp
@@ -1,4 +1,6 @@
+
 #include <string>
+#include <cstring>
 #include <iostream>
 #include <thread>
 #include <sstream>
@@ -10,6 +12,8 @@
 static struct mg_serve_http_opts s_http_server_opts;
 using namespace std;
 
+static int mg_http_get_request_len(const char* s, int buf_len);
+
 struct Files {
 	std::string file1;
 	std::string file2;
@@ -17,20 +21,55 @@ struct Files {
 	int size;
 };
 
-Files extractData(const char* buf) {
+Files extractData(const char* buf, size_t size) {
 	Files files;
 	std::stringstream s;
 	s << buf;
 	string line;
-	for (int i = 0; i < 9; i++) {
+	for (int i = 0; i < 8; i++) {
 		std::getline(s, line);
-		if (i == 4) {
+		if (i == 5) {
 			files.size = stoi(line.substr(line.find(': '), line.length()));
 		}
 	}
-	std::stringstream newS(line.substr(0, files.size));
-	getline(newS, files.file1, ':');
-	getline(newS, files.file2, ':');
+	string line2;
+	line = "";
+	while (getline(s, line2))
+	{
+		line += line2;
+	}
+	line = s.str();
+	string st = line.substr(mg_http_get_request_len(buf, size), files.size);
+	std::stringstream newS(st.substr(0, files.size));
+	//strtok data
+	//char* copyOfData;
+	//strcpy(copyOfData,newS.c_str());
+	//char* pch;
+	////char args[3];
+	//pch = strtok(copyOfData, ":");
+	//int i = 0;
+	//while (pch != NULL)
+	//{
+	//	//args[i] = pch;
+	//	pch = strtok(NULL, ":");
+	//	i++;
+	//}
+	st = "";
+	getline(newS, st);
+	while (!st.compare("\n"))
+	{
+		files.file1 += st;
+		getline(newS, st);
+	}
+
+	getline(newS, st);
+	getline(newS, st);
+	while (!st.compare("\n"))
+	{
+		files.file2 += st;
+		getline(newS, st);
+	}
+	getline(newS, st);
 	getline(newS, files.algo);
 	return files;
 }
@@ -40,8 +79,8 @@ void convertVecrtorToJASON(std::vector<AnomalyReport>& reports, string& s) {
 	json << "[" << endl;
 	for (AnomalyReport report : reports) {
 		json << " {" << std::endl;
-		json << "  \"timeStep\":\"" << report.timeStep << "\"" << "," << endl;
-		json << "  \"columns\":\"" << report.description << "\"" << endl;
+		json << "  \"timeStep\":" << report.timeStep << "," << endl;
+		json << "  \"columns\":" << report.description << endl;
 		json << " }," << endl;
 	}
 	json << "];";
@@ -49,9 +88,9 @@ void convertVecrtorToJASON(std::vector<AnomalyReport>& reports, string& s) {
 }
 
 static void ev_handler(struct mg_connection* nc, int ev, void* p) {
-	
+
 	if (ev == MG_EV_HTTP_REQUEST) {
-		Files files = extractData(nc->recv_mbuf.buf,nc->recv_mbuf.len);
+		Files files = extractData(nc->recv_mbuf.buf, nc->recv_mbuf.len);
 		TimeSeriesAnomalyDetector* detector;
 		if (files.algo.compare("hybrid")) {
 			detector = new HybridAnomalyDetector();
@@ -61,13 +100,18 @@ static void ev_handler(struct mg_connection* nc, int ev, void* p) {
 		}
 		std::thread l([files, detector]() -> void {
 			TimeSeries t(files.file1.c_str());
+			cout << "file1: " << files.file1.c_str() << endl;
 			detector->learnNormal(t); });
 		TimeSeries ts(files.file2.c_str());
 		l.join();
 		std::vector<AnomalyReport> v = detector->detect(ts);
 		string s;
 		convertVecrtorToJASON(v, s);
-		mg_printf(nc, s.c_str());
+		struct http_message* hm = (struct http_message*) p;
+
+		hm->message.p = s.c_str();
+		hm->message.len = s.length();
+		mg_serve_http(nc, hm , s_http_server_opts);
 		delete detector;
 		//mg_serve_http(nc, (struct http_message*) p, s_http_server_opts);
 	}
@@ -77,10 +121,10 @@ int initServer(int port) {
 	struct mg_mgr mgr;
 	struct mg_connection* nc;
 	std::string portToChar = std::to_string(port);
-	static char const *sPort = portToChar.c_str();
+	static char const* sPort = portToChar.c_str();
 	mg_mgr_init(&mgr, NULL);
 	std::cout << "Starting web server on port " << sPort << std::endl;
-	nc = mg_bind (&mgr, sPort, ev_handler);
+	nc = mg_bind(&mgr, sPort, ev_handler);
 	if (nc == NULL) {
 		std::cout << "failed to create listener" << std::endl;
 	}
